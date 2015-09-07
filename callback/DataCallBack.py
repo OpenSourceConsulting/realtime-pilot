0a35d389d87112fce705b77f50e3ec367b4bf0f2
Ji-Woong Choi <ienvyou@gmail.com>
2015-09-07 20:54:31 +0900

    Create a new DataCallBack python class

---
+++ callback/DataCallBack.py
@@ -0,0 +1,12 @@
+#DataCallBack.py
+import sys, os
+
+class CallBack:
+	def __init__(self):
+		self.version = 0.1
+
+	def _callback(self):
+		rstBody = '[' + self.process_idle.name + ', ' + str(self.process_idle.pid) + '], Class Callback === ' + str(m.body) + '\n\n'
+		print rstBody
+
+		#self.channel.basic_publish(arg_body.replace("KR", x), exchange_name, arg_rky)
