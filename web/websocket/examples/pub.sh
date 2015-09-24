82b34f322e0bec2813fc443b3118fba30d59a4af
Ji-Woong Choi <ienvyou@gmail.com>
2015-09-24 17:37:25 +0900

    Remove test web socket application

--- web/websocket/examples/pub.sh
+++
@@ -1,3 +0,0 @@
-python WebSocket_pub.py 10004 DATA_XKRX-CS-KR &
-python WebSocket_pub.py 10002 XKRX-CS-KR &
-python WebSocket_pub.py 10003 KR &
--- web/websocket_src/src/commands.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H_COMMANDS
-#define _H_COMMANDS
-
-#include "config.h"
-
-int prepare_commands(config_main_t *config);
-int release_commands(config_main_t *config);
-
-#endif //_H_COMMANDS
